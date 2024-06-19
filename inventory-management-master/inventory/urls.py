96b1372017ea3af0a20d5198cb478e66e9c273ce
salrealTeknet <salihuabduljawwad.a@gmail.com>
2024-06-19 23:48:27 +0100

    corrected

--- inventory-management-master/inventory/urls.py
+++ inventory-management-master/inventory/urls.py
@@ -6,7 +6,7 @@ from .views import about_to_expire
 urlpatterns = [
     path('inventory/', views.StockListView.as_view(), name='inventory'),
     path('new', views.StockCreateView.as_view(), name='new-stock'),
-    path('stock/<pk>/edit', views.StockUpdateView.as_view(), name='edit-stock'),
+    path('inventory/stock/<pk>/edit', views.StockUpdateView.as_view(), name='edit-stock'),
     path('stock/<pk>/delete', views.StockDeleteView.as_view(), name='delete-stock'),
     path('about-to-expire/', views.about_to_expire, name='about_to_expire'),
     path('download-csv/', views.download_csv, name='download-csv'),
