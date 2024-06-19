15624a0a3d935fa2e3f37bca5ec233a5172c8739
salrealTeknet <salihuabduljawwad.a@gmail.com>
2024-06-20 00:30:36 +0100

    corrected

--- inventory-management-master/core/settings.py
+++ inventory-management-master/core/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'                     # bootstrap template crispy-form uses
 
